f51339467a56a0540aa7d3e37a8121c57982164e
the <w180112@gmail.com>
2021-01-07 15:50:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Common utility for Linux system software programming
+
+[![BSD license](https://img.shields.io/badge/License-BSD-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+[![Build Status](https://travis-ci.org/w180112/vRG.svg?branch=master)](https://travis-ci.org/w180112/vRG)
+
+This is a Linux userspace system software programmimg utility. Programmers can easily compile them into a static library and use provided APIs in their project.
+
+## How to use:
+
+Git clone this repository
+
+	# git clone https://github.com/w180112/libutil.git
+
+Type
+
+	# cd libutil
+
+Type make to compile
+
+	# make
+
+Then you can find there is a static library in the same directory.
+
+To remove the binary files
+
+	# make clean
---
+++ util.h
@@ -0,0 +1,39 @@
+/*\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+  UTIL.H
+    the common utilities of all files are saved in this file.
+/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\*/
+
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*----------------------------------------------------
+ * TTY_PRINT() macro
+ *---------------------------------------------------*/
+#define TTY_PRINT   OSS_PRINT
+
+extern char *GetStrTok(char **cpp, char *delimiters);
+extern int  get_local_ip(U8 *ip, char *sif);
+extern int  set_local_ip(char *ip_str, char *sif);
+
+extern U8	*DECODE_U16(U16 *val, U8 *mp);
+extern U8	*DECODE_U24(U32 *val, U8 *mp);
+extern U8	*DECODE_U32(U32 *val, U8 *mp);
+
+extern U8	*ENCODE_U16(U8 *mp, U16 val);
+extern U8	*ENCODE_U24(U8 *mp, U32 val);
+extern U8	*ENCODE_U32(U8 *mp, U32 val);
+
+extern void PRINT_MESSAGE(unsigned char*, int);
+extern U16 	ADD_CARRY_FOR_CHKSUM(U32 sum);
+extern U16 	CHECK_SUM(U32);
+extern void OSS_PRINT(const char *, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
